9849b18e58dd75c57f55a095138dad759ad2f4e4
Javier Gonzalez <javgon2306@gmail.com>
2024-10-15 14:02:35 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+function App(){
+  return(
+    <>
+      <h1>App</h1>
+      <select name="" id="">
+        <option value="fire">Casa en llamas</option>
+        <option value="futurama">Futurama</option>
+        <option value="history">History Channel</option>
+        <option value="matrix">Matrix</option>
+        <option value="philosoraptor">Philosoraptor</option>
+        <option value="samart">Smart Guy</option>
+      </select>
+      <br/>
+      <input type="text"  placeholder="linea 1"/> <br />
+      <input type="text" placeholder="linea 2"/> <br />
+      <button>Exportar</button>
+      <div>
+        <span>Linea 1</span>
+        <span>Linea 2</span>
+      </div>
+    </>
+  )  
+}
+
+export default App;
